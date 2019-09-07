625160bcbd0f607ff020b617cca416ed8649e620
TataStrive <makchamp1@gmail.com>
2019-09-07 13:06:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button buttonWebView,buttonLoadHtml;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonWebView=findViewById(R.id.buttonwebpage);
+        load
+    }
+}
